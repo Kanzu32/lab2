f7a6b061aafa15d1d7abdf079cf4470a53cfa8e3
Bob (KozyavinMS) <kzmaxim256@gmail.com>
2023-06-21 15:11:49 +0300

    code: максимум

--- main.cpp
+++ main.cpp
@@ -10,5 +10,6 @@ int main() {
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n'
-     << "MIN: " << min(min(min(a + b, a - b), a * b), a / b);
+     << "MIN: " << min(min(min(a + b, a - b), a * b), a / b)
+     << "MAX: " << max(max(max(a + b, a - b), a * b), a / b);
 }
